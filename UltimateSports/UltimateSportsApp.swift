fbb7e94c5c06a73092655921b311188cc3bfea83
Khavi18 <khavishini18@gmail.com>
2024-07-20 18:55:31 +0800

    Initial Commit

---
+++ UltimateSports/UltimateSportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateSportsApp.swift
+//  UltimateSports
+//
+//  Created by Khavishini on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct UltimateSportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
